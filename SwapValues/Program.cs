8ccbddf55eb8349c4eab7194c679ec139190492b
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-07-04 12:16:17 +0300

    Add project files.

---
+++ SwapValues/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+    public class Swapper
+    {
+        public object[] Arguments { get; private set; }
+
+        public Swapper(object[] args)
+        {
+            if (args.Length != 2)
+            {
+                throw new ArgumentException();
+            }
+            Arguments = args;
+        }
+
+        public void SwapValues()
+        {
+            object[] args = new[] { Arguments[0], Arguments[1] };
+
+            object temp = args[0];
+            args[0] = args[1];
+            args[1] = temp;
+            Arguments = args;
+        }
+    }
+}
